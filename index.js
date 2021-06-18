313670b84a07e116ec4b623a0c279e80947ebd92
Jasper Concepcion <jasper@acadarena.com>
2021-06-18 20:49:20 +0700

    chore

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 var cors_proxy = require("cors-anywhere");
 cors_proxy
   .createServer({
-    originWhitelist: [], // Allow all origins
+    originWhitelist: [], // Allow all origins2
     removeHeaders: ["cookie", "cookie2"],
   })
   .listen(port, host, function () {
